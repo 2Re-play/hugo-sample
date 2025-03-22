62de19703209f266b0cf5eb921a476951b59ebbc
2Re-play <jaehyun.l@webtoonscorp.com>
2025-03-22 23:38:54 +0900

    hugo test

---
+++ public/searchindex.en.js
@@ -0,0 +1,26 @@
+var relearn_searchindex = [
+  {
+    "breadcrumb": "hugo sample site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Categories",
+    "uri": "/categories/index.html"
+  },
+  {
+    "breadcrumb": "",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "hugo sample site",
+    "uri": "/index.html"
+  },
+  {
+    "breadcrumb": "hugo sample site",
+    "content": "",
+    "description": "",
+    "tags": [],
+    "title": "Tags",
+    "uri": "/tags/index.html"
+  }
+]
